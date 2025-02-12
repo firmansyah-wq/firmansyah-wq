5c3b6f8d38f2cbc31a75d5b1218b61f505ad5d68
fabio <fabiofirmansyah@7.com>
2025-02-12 08:10:17 +0700

    Njir

---
+++ calc.lua
@@ -0,0 +1,40 @@
+--defines a variables
+operators = {'+', '-', '*', '/'}
+
+print("enter first number: ")
+num1 = io.read()
+print("enter operators: ")
+operations = io.read()
+print("enter second number:")
+num2 = io.read()
+
+
+if operations == '+' then 
+  print(num1+num2)
+elseif operations == '-' then
+  print(num1-num2)
+elseif operations == '*' then
+  print(num1*num2)
+elseif operations == '/' then
+  print(num1/num2)
+
+else 
+  print("pick something else")
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
